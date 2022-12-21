4df409852dff37754796c98c099ced9655f88e49
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2022-12-21 11:32:41 -0300

    Adicionei: - Uma função de adição e outra de subtração - Um if para quando o usuario quiser (o "deu certo" é temporario)

--- contas.cpp
+++ contas.cpp
@@ -4,17 +4,31 @@ int multipl(int numero1, int multiplic) {
 	return numero1 * multiplic;
 }
 
+int adição(int numero1, int numero2) {
+	return numero1 + numero2;
+}
+
+int subtração(int numero1, int numero2) {
+	return numero1 - numero2;
+}
+
 int main() {
-	int num;
+	int num1;
+	int num2;
+	char escolha;
 
-	std::cout << "Insira um numero e eu irei te dizer o dobro e o triplo do numero escolhido.\n";
-	std::cin >> num;
+	std::cout << "Insira dois numeros.\n";
+	std::cin >> num1 >> num2;
 	
-	int dobro = multipl(num, 2);
-	int triplo = multipl(num, 3);
+	std::cout << "Você gostaria de efetuar uma [A]dição, [S]ubtração ou uma [M]ultiplicação?\n";
+	std::cin >> escolha;
 
-	std::cout << "O dobro de " << num << " é " << dobro << std::endl;
-	std::cout << "O triplo de " << num << " é " << triplo << std::endl;
+	if(escolha == 'A' || escolha == 'S' || escolha == 'M') {
+		std::cout << "Deu certo\n";
+	}
+
+	//std::cout << num1 << " + " << num2 << " = " << adição(num1,num2) << std::endl;
+	//std::cout << num1 << " - " << num2 << " = " << subtração(num1,num2) << std::endl;
 	
 	return 0;
 }
