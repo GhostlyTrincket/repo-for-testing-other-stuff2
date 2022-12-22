dda7598671b6c59a1448424673f15ca427b6683c
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2022-12-22 15:41:05 -0300

    Adicionei a função 'Divisão'.

--- contas.cpp
+++ contas.cpp
@@ -12,15 +12,27 @@ int subtração(int numero1, int numero2) {
 	return numero1 - numero2;
 }
 
+float divisão(float numero1, float numero2) {
+	if(numero1 == 0 || numero2 == 0) {
+		//404 porque isso mostra que deu erro.
+		return 404;
+	} else {
+		//eu sei que, por exemplo, se eu colocar "1/4" vai me dar um valor do tipo float.
+		//mas por enquanto, sem dor de cabeça. Eu me preocupo com isso mais tarde.
+		return numero2 / numero1;
+	}
+}
+
 int main() {
 	int num1;
 	int num2;
+	float resultDivi;
 	char escolha;
 
 	std::cout << "Insira dois numeros.\n";
 	std::cin >> num1 >> num2;
 	
-	std::cout << "Você gostaria de efetuar uma [A]dição, [S]ubtração ou uma [M]ultiplicação, com estes numeros?\n";
+	std::cout << "Você gostaria de efetuar uma [A]dição, [S]ubtração, [M]ultiplicação ou uma [D]ivisão, com estes numeros?\n";
 	std::cin >> escolha;
 
 	if(escolha == 'A') {
@@ -31,7 +43,16 @@ int main() {
 	
 	if(escolha == 'M') {
 		std::cout << num1 << " X " << num2 << " = " << multipl(num1, num2) << std::endl;
+	} else if(escolha == 'D') {
+		resultDivi = divisão(num1,num2);
+
+		if(resultDivi == 404) {
+			std::cout << "Houve um erro ou dividir os numeros dados: certifique-se de não ter inserido 0.\n";
+		} else {
+			std::cout << num2 << " % " << num1 << " = " << resultDivi << std::endl;
+		}
 	}
 
 	return 0;
 }
+
