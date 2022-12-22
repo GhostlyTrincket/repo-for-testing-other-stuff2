665c43f1a9ec988d25f7a630ecbcffba125ab34f
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2022-12-22 16:22:40 -0300

    Melhorei a função de dividir. Agora, se algum valor inserido der 0, irá dar uma mensagem de erro no lugar do resultado. Proximo objetivo: diminuir o tamanho do main()

--- contas.cpp
+++ contas.cpp
@@ -14,24 +14,33 @@ int subtração(int numero1, int numero2) {
 
 float divisão(float numero1, float numero2) {
 	if(numero1 == 0 || numero2 == 0) {
-		//404 porque isso mostra que deu erro.
-		return 404;
+		std::cout << "ERRO.\nCODIGO DE SAÍDA:"; 
+		return 1;
 	} else {
-		//eu sei que, por exemplo, se eu colocar "1/4" vai me dar um valor do tipo float.
-		//mas por enquanto, sem dor de cabeça. Eu me preocupo com isso mais tarde.
 		return numero2 / numero1;
 	}
 }
 
+/*
+	objetivo: transformar a função
+*/
+
+/*char tipoOperação(char resutado) {
+	char operação;
+	std::cout << "me dia Qual será a operação\n".
+	cin >> operação;
+	
+	return operação;
+}*/
+
 int main() {
 	int num1;
 	int num2;
-	float resultDivi;
 	char escolha;
 
 	std::cout << "Insira dois numeros.\n";
 	std::cin >> num1 >> num2;
-	
+
 	std::cout << "Você gostaria de efetuar uma [A]dição, [S]ubtração, [M]ultiplicação ou uma [D]ivisão, com estes numeros?\n";
 	std::cin >> escolha;
 
@@ -44,15 +53,8 @@ int main() {
 	if(escolha == 'M') {
 		std::cout << num1 << " X " << num2 << " = " << multipl(num1, num2) << std::endl;
 	} else if(escolha == 'D') {
-		resultDivi = divisão(num1,num2);
-
-		if(resultDivi == 404) {
-			std::cout << "Houve um erro ou dividir os numeros dados: certifique-se de não ter inserido 0.\n";
-		} else {
-			std::cout << num2 << " % " << num1 << " = " << resultDivi << std::endl;
-		}
+		std::cout << num2 << " % " << num1 << " = " << divisão(num1,num2) << std::endl;
 	}
 
 	return 0;
 }
-
