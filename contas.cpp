b4b2f77e5a31f5dca987f621fab3a747884e1395
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2022-12-26 11:16:21 -0300

    Eu somente alterei os nomes das funções para algo mais curto.

--- contas.cpp
+++ contas.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 
-int multipl(int numero1, int multiplic) {
+int multiplicar(int numero1, int multiplic) {
 	return numero1 * multiplic;
 }
 
-int adição(int numero1, int numero2) {
+int somar(int numero1, int numero2) {
 	return numero1 + numero2;
 }
 
-int subtração(int numero1, int numero2) {
+int subtrair(int numero1, int numero2) {
 	return numero1 - numero2;
 }
 
-float divisão(float numero1, float numero2) {
+float dividir(float numero1, float numero2) {
 	if(numero1 == 0 || numero2 == 0) {
 		std::cout << "ERRO.\nCODIGO DE SAÍDA:"; 
 		return 1;
@@ -20,7 +20,6 @@ float divisão(float numero1, float numero2) {
 		return numero2 / numero1;
 	}
 }
-
 
 int main() {
 	int num1;
@@ -35,16 +34,17 @@ int main() {
 
 	//eu gosto de parear as operações
 	if(escolha == 'A') {
-		std::cout << num1 << " + " << num2 << " = " << adição(num1,num2) << std::endl;
+		std::cout << num1 << " + " << num2 << " = " << somar(num1,num2) << std::endl;
 	} else if(escolha == 'S') {
-		std::cout << num1 << " - " << num2 << " = " << subtração(num1,num2) << std::endl;
+		std::cout << num1 << " - " << num2 << " = " << subtrair(num1,num2) << std::endl;
 	}
 	
 	if(escolha == 'M') {
-		std::cout << num1 << " X " << num2 << " = " << multipl(num1, num2) << std::endl;
+		std::cout << num1 << " X " << num2 << " = " << multiplicar(num1, num2) << std::endl;
 	} else if(escolha == 'D') {
-		std::cout << num2 << " % " << num1 << " = " << divisão(num1,num2) << std::endl;
+		std::cout << num2 << " % " << num1 << " = " << dividir(num1,num2) << std::endl;
 	}
 
 	return 0;
 }
+
