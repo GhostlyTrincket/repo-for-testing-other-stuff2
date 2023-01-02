3c236a4eb2bda5257fb0f4b2fba3a533e15c1320
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-01-02 10:02:05 -0300

    Adicionei os 'ifndef' em caso de eu querer melhorar este programa no futuro.

--- quadratica/verificador.h
+++ quadratica/verificador.h
@@ -1,10 +1,11 @@
-#include <iostream>
-
-//esse arquivo vai servir para pegar falhas.
+//esse arquivo vai servir para definir as funções que irão pegar as falhas.
 //eu acho que não deveria fazer isso em um header
+#ifndef VERIFICADOR_H
+#define VERIFICADOR_H
 
 int verificarA(int a);
 
+int verificarDelta(int delta);
 //eu sinto que... eu vou precisar de mais alguma função aqui...
 
-int verificarDelta(int delta);
+#endif
