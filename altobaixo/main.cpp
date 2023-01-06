ec911515dca9979184db4df45475c83a58c8c580
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-01-06 12:37:00 -0300

    Adicionei a estrutura básica desse meu teste.

---
+++ altobaixo/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+	int seuNum;
+	std::cout << "Tente adivinhar o numero em que estou pensando (5) e, eu te direi se você está QUENTE ou FRIO.\n";
+	std::cout << "Insira o  numero que você está pensando aqui:";
+
+	std::cin >> seuNum;
+	
+	return 0;
+}
