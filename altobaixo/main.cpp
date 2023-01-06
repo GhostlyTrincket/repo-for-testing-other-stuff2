da8fcc5244eb056ef5217ed934d671f373a27c90
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-01-06 13:16:35 -0300

    Alterei um pouco a estrutura do main. Adicionei uma variável para limitar as tentativas que o jogador terá. Irei mecher nisso no futuro. Adicionei duas fowards functions, também.

--- altobaixo/main.cpp
+++ altobaixo/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
+void verificarNum(int seuNum); 
+
+int verficarTentativas(int tentativas);
+
 int main() {
 	int seuNum;
-	std::cout << "Tente adivinhar o numero em que estou pensando (5) e, eu te direi se você está QUENTE ou FRIO.\n";
-	std::cout << "Insira o  numero que você está pensando aqui:";
+	int tentativas = 3;
+	std::cout << "Tente adivinhar o numero em que estou pensando. Você tem " << tentativas << " tentativas.\n";
+	std::cout << "Insira o numero que você está pensando aqui:";
 
 	std::cin >> seuNum;
 	
+	verificarNum(seuNum);
 	return 0;
 }
