c9f0176ccdc3c24aaf10e315da42cf7f3b99b7cd
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-01-10 17:58:55 -0300

    Adicionei a estrutura basica do programa.

---
+++ ifs/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int pegarNum() {
+	int number;
+
+	std::cin >> number;
+
+	return number;
+}
+
+int main() {
+//Perfect Square Trinomial program.
+	int a; //a != 0
+	int b; //b = R'' 
+	int c; //c = R''
+
+	std::cout << "
+	return 0;
+}
