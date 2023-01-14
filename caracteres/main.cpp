43cccec5cebe0ab55007ec965dac11fe8353bf73
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-01-14 13:52:21 -0300

    Adicionei um arquivo que lê um caractere.

---
+++ caracteres/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	char palavra;
+
+	std::cout << "Insira uma palavra aqui:\n";
+	std::cin >> palavra;
+
+	std::cout << "Você digitou: " << palavra << std::endl;
+	std::cout << "aaa" << '\n';
+
+	return 0;
+}
