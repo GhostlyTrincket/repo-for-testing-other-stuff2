84789cbb24b954d5dd8efdb04d3bbfff18267ca2
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-01-16 09:56:38 -0300

    Criei um cabeçalho que contem as funções necessárias.

---
+++ strings/converter.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string>
+
+int sumNameAge(int name, int age);
+
+//int stringToInt(std::string stringGiven);
+int stringToInt(std::string strinGiven);
+
+void printResult(int sum);
+
+#endif
