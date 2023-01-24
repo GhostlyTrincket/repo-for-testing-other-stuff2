82442f34cfba9dfc5e996bf7b4dc280bab1ca84b
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-01-24 09:56:57 -0300

    Criei um programa para testar o operadore binario '^'.

---
+++ mani-bits/bitwiseXor.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bitset>
+
+int main() {
+	std::bitset<8> binario1 = 0b110'111'00; //binario de 220
+	std::bitset<8> binario2 = 0b101'101'00; //binario de 180
+
+	std::cout << "Binario de 220: " << binario1 << '.' << std::endl;
+	std::cout << "Binario de 180: " << binario2 << '.' << std::endl;
+	std::cout << "Operação XOR dos dois valores: " << (binario1 ^ binario2) << std::endl;
+
+	return 0;
+}
