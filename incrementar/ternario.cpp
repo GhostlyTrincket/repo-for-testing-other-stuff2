5ae419976fe883aaab821a784a47846d156a386e
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-01-31 09:56:18 -0300

    Criei um arquivo para eu poder testar o operador ternario.

---
+++ incrementar/ternario.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	int randNum = 116;
+	//este arquivo vai ser sobre o operador ternario.
+
+	std::cout << ((randNum < 19) ? "É menor que 19." : "É maior que 19.") << std::endl;
+
+	return 0;
+}
