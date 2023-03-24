cedefc318e27ce4cc1e83a818427e3e1d9050ff2
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-03-24 16:17:33 -0300

    Na função main, eu decidi trazer tudo que estava na função 'obterValoresMedia()' para dentro do loop. Já na função notasAnuais(), do arquivo media, eu adicionei um comentario para dar uma explicação para clarear os motivos de ter um for().

--- media-total/main.cpp
+++ media-total/main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include "media.h"
+#include "userInput.h"
 
 int main() {
 	std::cout << "Olá, vamos calcular a média de suas notas?\n";
 
 	while(true) {
-		obterValoresMedia();
+//TODO: Tentar arranjar de diminuir o máximo possivel essa função.
+		std::cout << "Para calcularmos a média de suas notas, por favor me diga quantas provas ocorreram.\n";
+		int quantiProvas = obterProvas();
+
+		double notaTotal = notasAnuais(quantiProvas);
+		double notaFinal = calcMedia(notaTotal,quantiProvas);
+
+		mostrNotas(notaTotal,notaFinal);
+
+//Talvez eu não precise disso daqui...
+		std::cout << "Você gostaria de sair do programa? [S/N].\n";
+		char userInput = obterInput();
+
+//eu não sei se tem outro jeito de terminar esse programa que não seja usando o std::exit
+//update: eu descobri... outro jeito.
+
+		if(userInput == 'S')
+			break;
 	}
 
 	return 0;
--- media-total/media.cpp
+++ media-total/media.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <cstdlib>
 #include "userInput.h"
 
 double calcMedia(double notaTotal, double provasPeloAno) {
 //(valor1 + valor2 + valor3 + valorN) / N
-
-//TODO: Descobrir se precisa arredondar ou não.
+//update: eu posso arredondar sim!
 
 	return notaTotal / provasPeloAno;
 }
@@ -13,8 +11,9 @@ double calcMedia(double notaTotal, double provasPeloAno) {
 double notasAnuais(int provasAno) {
 	double notaTotal = 0;
 
-	for(int i = 1; i <= provasAno; ++i) {
-		std::cout << "Me diga o quanto você tirou na " << i << "ª prova, por favor.\n";
+//loop para obter as nota(s) da(s) prova(s) pelo ano
+	for(int provaAtual = 1; i <= provasAno; ++provaAtual) {
+		std::cout << "Me diga o quanto você tirou na " << provaAtual << "ª prova, por favor.\n";
 		double notaProva = obterNota();
 
 		notaTotal += notaProva;
@@ -34,28 +33,4 @@ char obterOpção() {
 	std::cin >> opção;
 
 	return opção;
-}
-
-void obterValoresMedia() {
-
-//TODO: Tentar arranjar de diminuir o máximo possivel essa função.
-
-	std::cout << "Para calcularmos a média de suas notas, por favor me diga quantas provas ocorreram.\n";
-	int quantiProvas = obterProvas();
-
-	double notaTotal = notasAnuais(quantiProvas);
-	double notaFinal = calcMedia(notaTotal,quantiProvas);
-
-	mostrNotas(notaTotal,notaFinal);
-
-//Talvez eu não precise disso daqui...
-	std::cout << "Você gostaria de sair do programa? [S/N].\n";
-	char userInput = obterInput();
-
-//eu não sei se tem outro jeito de terminar esse programa que não seja usando o std::exit
-
-//TODO: Descobrir um jeito melhor de terminar o programa.
-
-	if(userInput == 'S')
-		std::exit(0);
 }
