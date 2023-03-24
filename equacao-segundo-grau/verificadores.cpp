6aac98e1d6019c61b27cf1284e58c79a4949ad6c
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-03-24 15:53:12 -0300

    Alterei o tipo de saida do 'exit()'

--- equacao-segundo-grau/verificadores.cpp
+++ equacao-segundo-grau/verificadores.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
+#include <cstdlib>
 
 void verifyA(double a) {
 //if a = 0 then...
 //F(0) = a * 0² + b * x + c
 //and then it stops being second degree, which is needed by default to calculate delta. 
-
 	if(a == 0.0) {
 		std::cout << "The value of 'a' is 0. Stopping calculus!\n";
-		exit(1);
+		std::exit(0);
 	}
 }
 
@@ -15,6 +15,6 @@ void verifyDelta(double delta) {
 	if(delta < 0.0) {
 //delta = i
 		std::cout << "Imaginary number detected, stopping calculus!\n";
-		exit(1);
+		std::exit(0);
 	}
 }
