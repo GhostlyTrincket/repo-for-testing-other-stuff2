795523c89112d24bb0f2dd642c1cb9e9fb120a83
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-03-30 15:07:36 -0300

    Corrigi um erro no loop for, antes estava 'i <= provaAno' mas agora está 'provaAtual <= provaAno

--- media-total/media.cpp
+++ media-total/media.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
+#include <iomanip>
 #include "userInput.h"
 
 double calcMedia(double notaTotal, double provasPeloAno) {
 //(valor1 + valor2 + valor3 + valorN) / N
-//update: eu posso arredondar sim!
-
 	return notaTotal / provasPeloAno;
 }
 
@@ -12,7 +11,7 @@ double notasAnuais(int provasAno) {
 	double notaTotal = 0;
 
 //loop para obter as nota(s) da(s) prova(s) pelo ano
-	for(int provaAtual = 1; i <= provasAno; ++provaAtual) {
+	for(int provaAtual = 1; provaAtual <= provasAno; ++provaAtual) {
 		std::cout << "Me diga o quanto você tirou na " << provaAtual << "ª prova, por favor.\n";
 		double notaProva = obterNota();
 
@@ -24,13 +23,6 @@ double notasAnuais(int provasAno) {
 
 void mostrNotas(double notaTotal, double notaFinal) {
 	std::cout << "Somando as notas das provas, você tirou " << notaTotal << '\n';
+	std::cout << std::setprecision(2);
 	std::cout << "A sua média foi de " << notaFinal << '\n';
-}
-
-char obterOpção() {
-	char opção = ' ';
-
-	std::cin >> opção;
-
-	return opção;
 }
