273a45d808e3376585237d83be59a534ab384870
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-03-30 15:16:05 -0300

    Retirei todos os comentarios da função main.

--- media-total/main.cpp
+++ media-total/main.cpp
@@ -6,7 +6,6 @@ int main() {
 	std::cout << "Olá, vamos calcular a média de suas notas?\n";
 
 	while(true) {
-//TODO: Tentar arranjar de diminuir o máximo possivel essa função.
 		std::cout << "Para calcularmos a média de suas notas, por favor me diga quantas provas ocorreram.\n";
 		int quantiProvas = obterProvas();
 
@@ -15,12 +14,8 @@ int main() {
 
 		mostrNotas(notaTotal,notaFinal);
 
-//Talvez eu não precise disso daqui...
 		std::cout << "Você gostaria de sair do programa? [S/N].\n";
 		char userInput = obterInput();
-
-//eu não sei se tem outro jeito de terminar esse programa que não seja usando o std::exit
-//update: eu descobri... outro jeito.
 
 		if(userInput == 'S')
 			break;
