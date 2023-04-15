877fffc3c4a273202a10bdef853f4e4da5adf8f2
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2023-04-15 14:46:21 -0300

    Renomeei os arquivos que eram chamados de 'calculo.cpp' e seu arquivo header.

---
+++ equacao-segundo-grau/calculation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+double calcDelta(double a, double b, double c) {
+//Delta = b² - 4 * a * c
+	return b * b - 4 * a * c;
+}
+
+void printResult(double x1, double x2) {
+	std::cout << "First root: " << x1 << '\n';
+	std::cout << "Second root: " << x2 << '\n';
+}
+
+void calcBashk(double a, double b, double delta) {
+//(-b) -+ SquareRootOf(Delta) / 2 * a
+//if delta = 0 then, there will be only one root.
+//if delta > 0 then, two roots.
+	double x1 = (-b + sqrt(delta)) / (2 * a);
+	double x2 = (-b - sqrt(delta)) / (2 * a);
+	
+	printResult(x1,x2);
+}
---
+++ equacao-segundo-grau/calculation.h
@@ -0,0 +1,10 @@
+#ifndef CALCULATION_H
+#define CALCULATION_H
+
+double calcDelta(double,double,double);
+
+void calcBashk(double,double,double);
+
+void printResult(double,double);
+
+#endif
